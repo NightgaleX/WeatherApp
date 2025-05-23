17b07b684c332179150eacc7a2e957ca5dea6a56
Oliver <Oliverwelamsson>
2025-05-23 13:03:42 +0200

    basic version of the weatherapp still missing a backend to communicate with however

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function WeatherApp() {
+  const [city, setCity] = useState("");
+
+  const [weather, setWeather] = useState(null);
+
+  const getWAppData = async () => {
+    const res = await fetch(`http://localhost:8080/weather/${city}`);
+    const data = await res.json();
+
+    setWeather(data);
+    console.log(city);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>rain or clear skies???</h2>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            getWAppData();
+          }
+        }}
+        placeholder="test your luck"
+      />
+      <button onClick={getWAppData}>Try it</button>
+
+      {weather && (
+        <div>
+          <p>Temperature: {weather.temperature}</p>
+          <p>Wind: {weather.wind}</p>
+          <p>Description: {weather.description}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default WeatherApp;
