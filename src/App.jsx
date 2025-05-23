9df7516c437a56e33ec8cec0879ab59a05afa263
Holgercode <hogga89@gmail.com>
2025-05-23 17:55:36 +0200

    updated with some pseudo handling thoughts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
 
@@ -14,30 +10,25 @@ function App() {
       </div>
       <div id="container">
         <div >
-          <input id="Searchbar" placeholder="Location" />
+          <input id="Searchbar" placeholder="Enter location" />
           <button id="Submit" type="submit">Search</button>
         </div>
       </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      async function getWeather() {
+        //input tas vid klick på submitknapp och sätts i inputvariabel
+        // inkommande data från input
+        //input blir city
+        //restAPI url till variabel
+        //fetchar rest API med input på cityvariabel
+        //errorhandling
+        //tar emot response
+        //behandlar json för print
+        //printa ut resultat 
+        // ?????
+        //PROFIT!!!1!1!1
+        const url = "http://goweather.xyz/weather/{city}"
+      const response = await fetch(url);
+
     </>
   )
 }
